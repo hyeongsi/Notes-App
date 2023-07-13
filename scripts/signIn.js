2522a909cc0ec85b2e53f2626c8475816dec6388
ParkSiHyeong <parksh4603@naver.com>
2023-07-14 01:00:56 +0900

    Feat: Sign in page, temp signin, up btn
    - 회원가입 창을 활용해서 로그인 창 생성하고
    중복된 css부분 하나로 합침
    
    - 임시로 홈에 회원가입, 로그인 페이지로 이동할 수 있도록 버튼 생성
    db 연동 후 로그인 정보에 따라 다르게 동작해야 해서 나중에 수정해야 함

--- scripts/signIn.js
+++ scripts/signIn.js
@@ -1,20 +1,15 @@
 $(document).ready(function () {
 	$("#id").on("blur", function () {
-		esnText($(this), $("#esntl_idtext"), "* 아이디: 필수정보 입니다.");
+		esnText($(this), $("#esntl_idtext"), "* 아이디를 입력해주세요");
 	});
 
 	$("#pw").on("blur", function () {
-		esnText($(this), $("#esntl_pwtext"), "* 비밀번호: 필수정보 입니다.");
-	});
-
-	$("#name").on("blur", function () {
-		esnText($(this), $("#esntl_nametext"), "* 이름: 필수정보 입니다.");
+		esnText($(this), $("#esntl_pwtext"), "* 비밀번호를 입력해주세요");
 	});
 
 	$(".btn__submit").on("click", function () {
 		esnText($("#id"), $("#esntl_idtext"), "* 아이디: 필수정보 입니다.");
 		esnText($("#pw"), $("#esntl_pwtext"), "* 비밀번호: 필수정보 입니다.");
-		esnText($("#name"), $("#esntl_nametext"), "* 이름: 필수정보 입니다.");
 	});
 });
 
